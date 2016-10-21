bf6e326410d228f43308fb01af25cfb9bac1deef
pyryjook <pyry@monday.fi>
2016-10-21 16:45:11 +0300

    add GA-script

--- server/app.js
+++ server/app.js
@@ -173,12 +173,29 @@ function renderHTML({ componentHTML, initialState, metaData, config }) {
         </head>
         <body>
         <div id="react-view">${componentHTML}</div>
-          <script type="application/javascript">
+        <script type="application/javascript">
             window.__CONFIG__ = ${JSON.stringify(config)};
             window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-          </script>
-
-          <script type="application/javascript" src="${config.staticUrl}/static/build/main.js"></script>
+        </script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-62552475-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <script>
+            (function(f,b){
+                var c;
+                f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
+                f._hjSettings={hjid:32935, hjsv:4};
+                c=b.createElement("script");c.async=1;
+                c.src="//static.hotjar.com/c/hotjar-"+f._hjSettings.hjid+".js?sv="+f._hjSettings.hjsv;
+                b.getElementsByTagName("head")[0].appendChild(c);
+            })(window,document);
+        </script>
+        <script type="application/javascript" src="${config.staticUrl}/static/build/main.js"></script>
         </body>
         </html>
     `;
