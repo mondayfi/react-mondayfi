578ebd47ac00e8ff5502b2b2fc3a1e9abab8a82f
pyryjook <pyry@monday.fi>
2016-10-21 16:38:24 +0300

    remove MC script

--- server/app.js
+++ server/app.js
@@ -179,7 +179,6 @@ function renderHTML({ componentHTML, initialState, metaData, config }) {
           </script>
 
           <script type="application/javascript" src="${config.staticUrl}/static/build/main.js"></script>
-          <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
         </body>
         </html>
     `;
