049a0e538cac9771a764623371f8e45c1eebe525
Samuel Klinkmann <samuel.klinkmann@sc5.io>
2017-04-04 21:59:55 +0300

    Fix

--- server/app.js
+++ server/app.js
@@ -181,11 +181,20 @@ function renderHTML({ componentHTML, initialState, metaData, config, url }) {
             window.__CONFIG__ = ${JSON.stringify(config)};
             window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
         </script>
+<!-- Page hiding snippet (recommended) -->
+<style>.async-hide { opacity: 0 !important} </style>
+<script>
+(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);
+})(window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-WFPHCDF':true});
+</script>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WFPHCDF');</script>
+
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WFPHCDF"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <script>
@@ -198,6 +207,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                 b.getElementsByTagName("head")[0].appendChild(c);
             })(window,document);
         </script>
+        
         <script type="application/javascript" src="${config.staticUrl}/static/build/main.js"></script>
         </body>
         </html>
