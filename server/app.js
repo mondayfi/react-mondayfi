53688ffea4a20e7c218646376377e8f47be5bee7
Samuel Klinkmann <samuel.klinkmann@sc5.io>
2017-04-04 21:11:25 +0300

    Optimize code

--- server/app.js
+++ server/app.js
@@ -185,7 +185,8 @@ function renderHTML({ componentHTML, initialState, metaData, config, url }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WFPHCDF');</script>
+})(window,document,'script','dataLayer','GTM-WFPHCDF');
+ga('require', 'GTM-5WZRVLP');</script>
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WFPHCDF"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <script>
